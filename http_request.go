5e114414e617effa33e7d4b621c93871ab0f0fd3
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-02-14 17:27:56 +0200

    fixed MakePostRequest to take map[string]interface

--- http_request.go
+++ http_request.go
@@ -11,10 +11,10 @@ import (
 
 const DEFAULT_REQUEST_TIMEOUT = 30
 
-func MakePostRequest(url string, data, headers map[string]string) (result map[string]interface{}, err error) {
+func MakePostRequest(url string, data map[string]interface{}, headers map[string]string) (result map[string]interface{}, err error) {
     req := request.NewRequest(new(http.Client))
     req.Client.Timeout = time.Duration(DEFAULT_REQUEST_TIMEOUT * time.Second)
-    req.Data = data
+    req.Json = data
     req.Headers = headers
 
     resp, err := req.Post(url)
