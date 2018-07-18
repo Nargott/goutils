7df44a571a7e5dbd7682561b677bbb87884a045f
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-07-18 15:04:07 +0300

    Added IsStringInArray func

---
+++ array.go
@@ -0,0 +1,16 @@
+package goutils
+
+import "strings"
+
+/**
+Returns true if given string is in array
+ */
+func IsStringInArray(str string, arr *[]string) bool {
+	for _, record := range *arr {
+		if strings.EqualFold(str, record) {
+			return true
+		}
+	}
+
+	return false
+}
