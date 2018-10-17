0aa7d50070ceef893c5ef972a7fee1947b3d51c7
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-10-17 10:45:31 +0300

    Fixed IsStringInArray to accept array as 2nd argument

--- array.go
+++ array.go
@@ -5,8 +5,8 @@ import "strings"
 /**
 Returns true if given string is in array
  */
-func IsStringInArray(str string, arr *[]string) bool {
-	for _, record := range *arr {
+func IsStringInArray(str string, arr []string) bool {
+	for _, record := range arr {
 		if strings.EqualFold(str, record) {
 			return true
 		}
