7521701e07871d051f74a968964e4a725fe2366c
Nargott <kiberpunk.mail@gmail.com>
2018-12-09 22:39:11 +0200

    Fixed UUIDToBase64 to use string as input param to prevent type mismatch with vendor

--- uuid_base64.go
+++ uuid_base64.go
@@ -6,9 +6,14 @@ import (
 )
 
 // UUIDToBase64 returns packed and Base64-encoded UUID as string
-func UUIDToBase64(u uuid.UUID) string {
+func UUIDToBase64(su string) (string, error) {
+	u, err := uuid.FromString(su)
+	if err != nil {
+		return "", err
+	}
+
 	bu, _ := u.MarshalBinary() // MarshalBinary does not returns error at all, so ignoring it
-	return base64.StdEncoding.EncodeToString(bu)
+	return base64.StdEncoding.EncodeToString(bu), nil
 }
 
 // Base64ToUuid returns unpacked base64-decoded and converted from binary UUID
--- uuid_base64_test.go
+++ uuid_base64_test.go
@@ -10,22 +10,46 @@ import (
 
 func TestUUIDToBase64(t *testing.T) {
 	tests := []struct {
-		param  uuid.UUID
-		result string
+		param       string
+		result      string
+		resultError error
 	}{
 		{
-			param:  uuid.FromStringOrNil("831e0004-2465-477f-a7b4-116cac875798"),
-			result: "gx4ABCRlR3+ntBFsrIdXmA==",
+			param:       "831e0004-2465-477f-a7b4-116cac875798",
+			result:      "gx4ABCRlR3+ntBFsrIdXmA==",
+			resultError: nil,
 		},
 		{
-			param:  uuid.FromStringOrNil("1af0994b-6901-4896-bef9-75ebac55c1ba"),
-			result: "GvCZS2kBSJa++XXrrFXBug==",
+			param:       "1af0994b-6901-4896-bef9-75ebac55c1ba",
+			result:      "GvCZS2kBSJa++XXrrFXBug==",
+			resultError: nil,
+		},
+		{
+			param:       "bad uuid",
+			result:      "",
+			resultError: fmt.Errorf("uuid: incorrect UUID length: bad uuid"),
+		},
+		{
+			param:       "1af0994bw6901w4896wbef9w75ebac55c1ba",
+			result:      "",
+			resultError: fmt.Errorf("uuid: incorrect UUID format 1af0994bw6901w4896wbef9w75ebac55c1ba"),
 		},
 	}
 
 	for _, test := range tests {
-		result := UUIDToBase64(test.param)
-
+		result, err := UUIDToBase64(test.param)
+		if err == nil && test.resultError != nil {
+			t.Errorf("unexpected <nil> error was occured")
+			continue
+		}
+		if err != nil && test.resultError == nil {
+			t.Errorf("unexpected error was occured: %s", err.Error())
+			continue
+		}
+		if err != nil && test.resultError != nil && !strings.EqualFold(err.Error(), test.resultError.Error()) {
+			t.Errorf("unexpected error was occured: %s, when expects: %s", err.Error(), test.resultError.Error())
+			continue
+		}
 		if !reflect.DeepEqual(test.result, result) {
 			t.Errorf("received value %s is not equal to expected value: %s", result, test.result)
 		}
