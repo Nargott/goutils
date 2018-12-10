d38178e13f8d4b91ac8176d5f92b3357af9d20c8
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-12-10 17:05:01 +0200

    Fixed GetFuncName method to use actual skip value

--- runtime_utils.go
+++ runtime_utils.go
@@ -8,7 +8,7 @@ import (
 
 func GetFuncName(skip int) (string, error) {
 	fpcs := make([]uintptr, 1)
-	_ = runtime.Callers(1, fpcs)
+	_ = runtime.Callers(skip, fpcs)
 	if fpcs[0] < 1 {
 		return "", fmt.Errorf("bad func pc ptr")
 	}
