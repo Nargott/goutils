8d1158b1f2149e4c97064262c177919074612981
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-12-10 16:59:43 +0200

    Added GetFuncName method

---
+++ runtime_utils.go
@@ -0,0 +1,22 @@
+package goutils
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+func GetFuncName(skip int) (string, error) {
+	fpcs := make([]uintptr, 1)
+	_ = runtime.Callers(1, fpcs)
+	if fpcs[0] < 1 {
+		return "", fmt.Errorf("bad func pc ptr")
+	}
+	caller := runtime.FuncForPC(fpcs[0] - 1)
+	path := strings.Split(caller.Name(), ".")
+	if len(path) < 1 {
+		return "", fmt.Errorf("bad path returned")
+	}
+
+	return path[len(path)-1], nil
+}
