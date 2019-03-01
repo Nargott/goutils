7e762585b37670148fe839384a0bc23f22db9bd1
Serhiy Korzun <kiberpunk.mail@gmail.com>
2019-03-01 13:43:12 +0200

    Added unstring convertion functions to UUID-base64

--- uuid_base64.go
+++ uuid_base64.go
@@ -5,8 +5,8 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-// UUIDToBase64 returns packed and Base64-encoded UUID as string
-func UUIDToBase64(su string) (string, error) {
+// StringUUIDToBase64 returns packed and Base64-encoded UUID as string
+func StringUUIDToBase64(su string) (string, error) {
 	u, err := uuid.FromString(su)
 	if err != nil {
 		return "", err
@@ -17,11 +17,27 @@ func UUIDToBase64(su string) (string, error) {
 }
 
 // Base64ToUuid returns unpacked base64-decoded and converted from binary UUID
-func Base64ToUuid(bs string) (string, error) {
+func Base64ToUuidString(bs string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(bs)
 	if err != nil {
 		return "", err
 	}
 
 	return uuid.FromBytesOrNil(b).String(), nil
+}
+
+// UUIDToBase64 returns packed and Base64-encoded UUID
+func UUIDToBase64(u uuid.UUID) string {
+	bu, _ := u.MarshalBinary() // MarshalBinary does not returns error at all, so ignoring it
+	return base64.StdEncoding.EncodeToString(bu)
+}
+
+// Base64ToUuid returns unpacked base64-decoded and converted from binary UUID
+func Base64ToUuid(bs string) (uuid.UUID, error) {
+	b, err := base64.StdEncoding.DecodeString(bs)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return uuid.FromBytesOrNil(b), nil
 }
--- uuid_base64_test.go
+++ uuid_base64_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestUUIDToBase64(t *testing.T) {
+func TestStringUUIDToBase64(t *testing.T) {
 	tests := []struct {
 		param       string
 		result      string
@@ -37,7 +37,7 @@ func TestUUIDToBase64(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		result, err := UUIDToBase64(test.param)
+		result, err := StringUUIDToBase64(test.param)
 		if err == nil && test.resultError != nil {
 			t.Errorf("unexpected <nil> error was occured")
 			continue
@@ -50,6 +50,90 @@ func TestUUIDToBase64(t *testing.T) {
 			t.Errorf("unexpected error was occured: %s, when expects: %s", err.Error(), test.resultError.Error())
 			continue
 		}
+		if !reflect.DeepEqual(test.result, result) {
+			t.Errorf("received value %s is not equal to expected value: %s", result, test.result)
+		}
+	}
+}
+
+func TestBase64ToUuidString(t *testing.T) {
+	tests := []struct {
+		param       string
+		result      string
+		resultError error
+	}{
+		{
+			param:       "gx4ABCRlR3+ntBFsrIdXmA==",
+			result:      "831e0004-2465-477f-a7b4-116cac875798",
+			resultError: nil,
+		},
+		{
+			param:       "GvCZS2kBSJa++XXrrFXBug==",
+			result:      "1af0994b-6901-4896-bef9-75ebac55c1ba",
+			resultError: nil,
+		},
+		{
+			param:       "bad base64 value",
+			result:      "",
+			resultError: fmt.Errorf("illegal base64 data at input byte 3"),
+		},
+		{
+			param:       "AAAAAAAAAAAAAAAAAAAAAA==", //clean uuid bytes value
+			result:      uuid.Nil.String(),
+			resultError: nil,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := Base64ToUuidString(test.param)
+		if err == nil && test.resultError != nil {
+			t.Errorf("unexpected <nil> error was occured")
+			continue
+		}
+		if err != nil && test.resultError == nil {
+			t.Errorf("unexpected error was occured: %s", err.Error())
+			continue
+		}
+		if err != nil && test.resultError != nil && !strings.EqualFold(err.Error(), test.resultError.Error()) {
+			t.Errorf("unexpected error was occured: %s, when expects: %s", err.Error(), test.resultError.Error())
+			continue
+		}
+		if !reflect.DeepEqual(test.result, result) {
+			t.Errorf("received value %s is not equal to expected value: %s", result, test.result)
+		}
+	}
+}
+
+func TestUUIDToBase64(t *testing.T) {
+	tests := []struct {
+		param       uuid.UUID
+		result      string
+		resultError error
+	}{
+		{
+			param:       uuid.FromStringOrNil("831e0004-2465-477f-a7b4-116cac875798"),
+			result:      "gx4ABCRlR3+ntBFsrIdXmA==",
+			resultError: nil,
+		},
+		{
+			param:       uuid.FromStringOrNil("1af0994b-6901-4896-bef9-75ebac55c1ba"),
+			result:      "GvCZS2kBSJa++XXrrFXBug==",
+			resultError: nil,
+		},
+		{
+			param:       uuid.FromStringOrNil("bad uuid"),
+			result:      "AAAAAAAAAAAAAAAAAAAAAA==",
+			resultError: fmt.Errorf("uuid: incorrect UUID length: bad uuid"),
+		},
+		{
+			param:       uuid.FromStringOrNil("1af0994bw6901w4896wbef9w75ebac55c1ba"),
+			result:      "AAAAAAAAAAAAAAAAAAAAAA==",
+			resultError: fmt.Errorf("uuid: incorrect UUID format 1af0994bw6901w4896wbef9w75ebac55c1ba"),
+		},
+	}
+
+	for _, test := range tests {
+		result := UUIDToBase64(test.param)
 		if !reflect.DeepEqual(test.result, result) {
 			t.Errorf("received value %s is not equal to expected value: %s", result, test.result)
 		}
@@ -85,7 +169,7 @@ func TestBase64ToUuid(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		result, err := Base64ToUuid(test.param)
+		result, err := Base64ToUuidString(test.param)
 		if err == nil && test.resultError != nil {
 			t.Errorf("unexpected <nil> error was occured")
 			continue
